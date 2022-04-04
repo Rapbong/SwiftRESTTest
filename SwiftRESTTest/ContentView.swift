af8dfcf8561356564a2e6c9b966a7c8a5a19b78c
Rapbong <rapbong@gmail.com>
2022-04-04 21:51:08 +0900

    Create project

---
+++ SwiftRESTTest/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftRESTTest
+//
+//  Created by Ilbeom on 2022/04/04.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftRESTTest/SwiftRESTTestApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftRESTTestApp.swift
+//  SwiftRESTTest
+//
+//  Created by Ilbeom on 2022/04/04.
+//
+//
+
+import SwiftUI
+
+@main
+struct SwiftRESTTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
